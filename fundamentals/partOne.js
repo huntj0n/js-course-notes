0fe07146917a7e09a8e0a544c7364ece60f3e8fe
huntj0n <hunt.jon19@gmail.com>
2023-01-23 16:51:58 -0700

    data types, variables, operators

---
+++ fundamentals/partOne.js
@@ -0,0 +1,33 @@
+// let js = "amazing";
+// // if (js === "amazing") alert("JS is FUN!");
+
+// console.log(40 + 8 + 23 - 10);
+
+// // VALUES //
+// console.log("jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJon = "teacher";
+
+// ======== DATA TYPES ========
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 34);
+// console.log(typeof "string");
+
+// javascriptIsFun = "YES!";
+//dynamic typing
+
+// ===== let, const, var =====
+let age = 30;
+age = 31;
