898a8d9b08234838f4e881a3c874b7761ad5a2ab
huntj0n <hunt.jon19@gmail.com>
2023-01-23 21:28:02 -0700

    basic operators and operator precedence

--- fundamentals/partOne.js
+++ fundamentals/partOne.js
@@ -29,5 +29,51 @@
 //dynamic typing
 
 // ===== let, const, var =====
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991
+// birthYear = 1990
+// you should be using let only when you know it will change. its good practice to have as little variable changes as possible. var is legacy and should be avoided, just know how it works.
+
+//  ===== BASIC OPERATORS =====
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = 2037 - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// // Comparison Operators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
