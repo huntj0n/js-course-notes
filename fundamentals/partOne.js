c61312a7cdc937a0991ca83b29408dacc59a9219
huntj0n <hunt.jon19@gmail.com>
2023-01-25 21:56:46 -0700

    truthy and falsey values

--- fundamentals/partOne.js
+++ fundamentals/partOne.js
@@ -185,30 +185,63 @@
 
 ////////////
 //=========== TYPE CONVERSION AND COERCION ===========
-const inputYear = "1991";
-console.log(inputYear + 19);
-console.log(Number(inputYear));
+// const inputYear = "1991";
+// console.log(inputYear + 19);
+// console.log(Number(inputYear));
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
-//JS can only convert to 3 types: string, number, or boolean. not undefined or NaN
+// console.log(String(23), 23);
+// //JS can only convert to 3 types: string, number, or boolean. not undefined or NaN
 
-//type coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3); //subtraction coerces to numbers
-console.log("23" + "10" + 3); //addition coerces to strings
-console.log("23" * "2");
-console.log("23" / "2");
+// //type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3); //subtraction coerces to numbers
+// console.log("23" + "10" + 3); //addition coerces to strings
+// console.log("23" * "2");
+// console.log("23" / "2");
 
-//guess what will print
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// //guess what will print
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
 
-let y = 2 + 3 + 4 + "5";
-console.log(y);
+// let y = 2 + 3 + 4 + "5";
+// console.log(y);
 
-let z = "10" - "4" - "3" - 2 + "5";
-console.log(z);
+// let z = "10" - "4" - "3" - 2 + "5";
+// console.log(z);
+
+//========= TRUTHY AND FALSEY VALUES ==========
+// 5 'falsy' values: 0, '', undefined, null, NaN
+// everything else is 'truthy'
+// ie, converts/boils down to true or false evaluations
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+// logical operators and if/else conditions
+const money = 10;
+if (money) {
+  console.log("dont spend it all");
+} else {
+  console.log("you should get a job");
+}
+
+// let height;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+//this is an example of a bug using this method, 0 evaluates to false even though it is technically 'defined'
