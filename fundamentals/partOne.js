c23a181e23d7bc182d4a3989ed3c045a27372363
huntj0n <hunt.jon19@gmail.com>
2023-01-25 20:59:45 -0700

    type coercion and conversion

--- fundamentals/partOne.js
+++ fundamentals/partOne.js
@@ -159,26 +159,56 @@
 
 //hint: Use an if/else statement
 
-const marksMass = 78;
-const marksHeight = 1.69;
-const johnsMass = 92;
-const johnsHeight = 1.95;
+// const marksMass = 78;
+// const marksHeight = 1.69;
+// const johnsMass = 92;
+// const johnsHeight = 1.95;
 
-// const marksMass = 95;
-// const marksHeight = 1.95;
-// const johnsMass = 85;
-// const johnsHeight = 1.76;
+// // const marksMass = 95;
+// // const marksHeight = 1.95;
+// // const johnsMass = 85;
+// // const johnsHeight = 1.76;
 
-const marksBMI = marksMass / marksHeight ** 2;
-const johnsBMI = johnsMass / johnsHeight ** 2;
+// const marksBMI = marksMass / marksHeight ** 2;
+// const johnsBMI = johnsMass / johnsHeight ** 2;
 
-let markHigherBMI = marksBMI > johnsBMI;
-if (markHigherBMI) {
-  console.log(
-    `Mark's BMI (${marksBMI}) is higher than John's BMI (${johnsBMI})`
-  );
-} else {
-  console.log(
-    `Mark's BMI (${marksBMI}) is lower than John's BMI (${johnsBMI})`
-  );
-}
+// let markHigherBMI = marksBMI > johnsBMI;
+// if (markHigherBMI) {
+//   console.log(
+//     `Mark's BMI (${marksBMI}) is higher than John's BMI (${johnsBMI})`
+//   );
+// } else {
+//   console.log(
+//     `Mark's BMI (${marksBMI}) is lower than John's BMI (${johnsBMI})`
+//   );
+// }
+
+////////////
+//=========== TYPE CONVERSION AND COERCION ===========
+const inputYear = "1991";
+console.log(inputYear + 19);
+console.log(Number(inputYear));
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+//JS can only convert to 3 types: string, number, or boolean. not undefined or NaN
+
+//type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3); //subtraction coerces to numbers
+console.log("23" + "10" + 3); //addition coerces to strings
+console.log("23" * "2");
+console.log("23" / "2");
+
+//guess what will print
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
+
+let y = 2 + 3 + 4 + "5";
+console.log(y);
+
+let z = "10" - "4" - "3" - 2 + "5";
+console.log(z);
