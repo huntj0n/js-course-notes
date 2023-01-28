881c2dec1f3af8aeab6110008493bee671c52269
huntj0n <hunt.jon19@gmail.com>
2023-01-27 20:39:57 -0700

    coding challenge 3

--- fundamentals/partOne.js
+++ fundamentals/partOne.js
@@ -271,26 +271,46 @@
 //======= BOOLEAN LOGIC/LOGICAL OPERATORS =======
 //and, or, not
 //!A, A AND B, A OR B, !A and B, A OR !B...
-const hasDriversLicense = true;
-const hasGoodVision = true;
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
 
-console.log(hasDriversLicense && hasGoodVision);
-console.log(hasDriversLicense || hasGoodVision);
-console.log(!hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense && hasGoodVision);
 
-const shouldDrive = hasDriversLicense && hasGoodVision;
+// const shouldDrive = hasDriversLicense && hasGoodVision;
 
-// if (shouldDrive) {
+// // if (shouldDrive) {
+// //   console.log("Sarah is able to drive!");
+// // } else {
+// //   console.log("Someone else should drive...");
+// // }
+
+// const isTired = true;
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (shouldDrive && hasGoodVision && !isTired) {
 //   console.log("Sarah is able to drive!");
 // } else {
 //   console.log("Someone else should drive...");
 // }
 
-const isTired = true;
-console.log(hasDriversLicense && hasGoodVision && isTired);
+/////////////////////////////////////////////////
+//======= CODING CHALLENGE #3 =======
+//
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the trophy.
 
-if (shouldDrive && hasGoodVision && !isTired) {
-  console.log("Sarah is able to drive!");
-} else {
-  console.log("Someone else should drive...");
-}
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Dont forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+// 3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks
+// 4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greate or equal 100 points. Otherwise, no team wins the trophy.
+
+// TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91, 110
+// TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// TEST DATA BONUS 2: Dolpphins score 97, 112 and 101. Koalas score 109, 95 and 106.
+
+const dolphinsScore = (96 + 108 + 89) / 3;
+const koalasScore = (88 + 91 + 110) / 3;
+
+// if()
