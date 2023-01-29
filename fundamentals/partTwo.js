1f985253faea49d812ed4f49d719b6d63ca95bc2
huntj0n <hunt.jon19@gmail.com>
2023-01-29 05:57:27 -0700

    functions

--- fundamentals/partTwo.js
+++ fundamentals/partTwo.js
@@ -5,3 +5,35 @@
 
 // if (passTest) hasDriverLicense = true;
 // if (hasDriversLicense) console.log('I can drive :D')
+
+//
+//======= FUNCTIONS =======
+//
+//functions are pieces of code that we can reuse over and over again. like a variable but for whole sections of code.
+
+//declaration
+// function logger() {
+//   console.log("my name is jonas");
+// }
+
+// //calling/running/invoking
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   //   console.log(apples, oranges);
+//   const juice = `juice with ${apples} apples and ${oranges} oranges`;
+
+//   return juice;
+// }
+//here, you have to capture the returned result of the function in order to use it. fruitProcessor(5,0) is the result of the function, but we need to tell the computer what to do with it
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//
+//===== FUNCTION DECLARATIONS VS EXPRESSIONS =====
+//
