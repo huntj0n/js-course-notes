e1d40187be30448acc7fff46ccfcfa87c07431b8
huntj0n <hunt.jon19@gmail.com>
2023-02-08 22:13:59 -0700

    jsf assignment 1

---
+++ js-fundamentals-assignments/script.js
@@ -0,0 +1,11 @@
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+const country = "America";
+const continent = "North America";
+const population = "350 million";
+
+console.log(counntry, continent, population);
