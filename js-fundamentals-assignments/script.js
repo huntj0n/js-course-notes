14e925b166cae9c2ef5b6f16644ca94fb136bdb1
huntj0n <hunt.jon19@gmail.com>
2023-02-11 19:02:35 -0700

    assignment: taking decisions, if/else

--- js-fundamentals-assignments/script.js
+++ js-fundamentals-assignments/script.js
@@ -6,6 +6,71 @@ assign their values according to your own country (population in millions)
 */
 const country = "America";
 const continent = "North America";
-const population = "350 million";
+const population = 350;
 
-console.log(counntry, continent, population);
+// console.log(counntry, continent, population);
+
+/*
+LECTURE: Data Types
+
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+const isIsland = false;
+let language;
+console.log(isIsland, population, country, language);
+
+/*
+LECTURE: let, const, and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+
+language = "English";
+// isIsland = true;
+
+/*
+LECTURE: Basic Operators
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+
+LECTURE: Strings and Template Literals
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax
+*/
+let halfPop = country / 2;
+console.log(population + 1);
+const popFinland = 6;
+population > popFinland ? console.log(true) : console.log(false);
+const averagePop = 33;
+populaiton > averagePop ? console.log(true) : console.log(false);
+
+const description = `${country} is in  ${continent} and its ${population} million people speak ${language}`;
+
+/*
+LECTURE: Taking Decisions: if / else Statements
+1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+*/
+population > averagePop
+  ? console.log(`${country}'s population is above average`)
+  : console.log(`${country}'s population is ${averagePop - population}`);
