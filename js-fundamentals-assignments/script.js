68b8e542df8a5ded8d5327d547cc30601c3809a7
huntj0n <hunt.jon19@gmail.com>
2023-02-17 05:25:38 -0700

    assignment: ternarry operators

--- js-fundamentals-assignments/script.js
+++ js-fundamentals-assignments/script.js
@@ -58,7 +58,7 @@ console.log(population + 1);
 const popFinland = 6;
 population > popFinland ? console.log(true) : console.log(false);
 const averagePop = 33;
-populaiton > averagePop ? console.log(true) : console.log(false);
+population > averagePop ? console.log(true) : console.log(false);
 
 const description = `${country} is in  ${continent} and its ${population} million people speak ${language}`;
 
@@ -172,3 +172,16 @@ switch (language) {
   default:
     console.log("Great language too :D");
 }
+
+/*
+LECTURE: The Conditional (Ternary) Operator
+    1. If your country's population is greater than 33 million, use the ternary operator
+    to log a string like this to the console: 'Portugal's population is above average'.
+    Otherwise, simply log 'Portugal's population is below average'. Notice how only
+    one word changes between these two sentences!
+    2. After checking the result, change the population temporarily to 13 and then to
+    130. See the different results, and set the population back to original
+*/
+population > 33
+  ? `${country}'s population is above average`
+  : `${country}'s population is below average`;
