60e47d99aa0cdafb66fc586cca9630a3e97f0110
huntj0n <hunt.jon19@gmail.com>
2023-02-17 06:06:19 -0700

    assignmen: functions calling other functions

--- js-fundamentals-assignments/script2.js
+++ js-fundamentals-assignments/script2.js
@@ -51,3 +51,19 @@ LECTURE: Arrow Functions
 const percentageOfWorld3 = (population) => (population / 7900) * 100;
 let popPerChina3 = percentageOfWorld3(1441);
 console.log(popPerChina3);
+
+/*
+LECTURE: Functions Calling Other Functions
+    1. Create a function called 'describePopulation'. Use the function type you
+    like the most. This function takes in two arguments: 'country' and
+    'population', and returns a string like this: 'China has 1441 million people,
+    which is about 18.2% of the world.'
+    2. To calculate the percentage, 'describePopulation' call the
+    'percentageOfWorld1' you created earlier
+    3. Call 'describePopulation' with data for 3 countries of your choice
+*/
+const describePopulation = function (country, population) {
+  percentage = popPerChina2(population);
+  return `${country} has ${population} million people which is ${percentage} of the world.`;
+};
+console.log(describePopulation("China", 1441));
