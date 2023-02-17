603444b85261a9c1eec46b882c1cf9236d46ee47
huntj0n <hunt.jon19@gmail.com>
2023-02-17 06:21:20 -0700

    assingment: intro to arrays

--- js-fundamentals-assignments/script2.js
+++ js-fundamentals-assignments/script2.js
@@ -67,3 +67,22 @@ const describePopulation = function (country, population) {
   return `${country} has ${population} million people which is ${percentage} of the world.`;
 };
 console.log(describePopulation("China", 1441));
+
+/*
+LECTURE: Introduction to Arrays
+    1. Create an array containing 4 population values of 4 countries of your choice.
+    You may use the values you have been using previously. Store this array into a
+    variable called 'populations'
+    2. Log to the console whether the array has 4 elements or not (true or false)
+    3. Create an array called 'percentages' containing the percentages of the
+    world population for these 4 population values. Use the function
+    'percentageOfWorld1' that you created earlier to compute the 4
+    percentage values
+*/
+const populations = [1441, 350, 55, 6];
+console.log(populations.length === 4);
+const percentages = [];
+for (i = 0; i <= populations.length; i++) {
+  percentages.push(percentageOfWorld2(i));
+}
+console.log(populations, percentages);
