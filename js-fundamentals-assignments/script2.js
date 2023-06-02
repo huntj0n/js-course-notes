5f793edf3dd20b104b8b12d97395e5b8f30a4c2e
huntj0n <hunt.jon19@gmail.com>
2023-06-01 21:27:42 -0600

    looping backwards and looping loops: assignment

--- js-fundamentals-assignments/script2.js
+++ js-fundamentals-assignments/script2.js
@@ -199,3 +199,29 @@ for (let percent = 0; percent < populations.length - 1; percent++) {
 }
 
 console.log(percentages2);
+
+/*
+LECTURE: Looping Backwards and Loops in Loops
+1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
+worry if it's too difficult for you! But you can still try to figure this out anyway �
+*/
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let n = 0; n < listOfNeighbours.length; n++) {
+  if (listOfNeighbours[n].length > 1) {
+    let arr = [];
+    for (neybor = 0; neybor < listOfNeighbours[n].length; neybor++) {
+      arr.push(listOfNeighbours[n][neybor]);
+    }
+    console.log(arr);
+  }
+}
