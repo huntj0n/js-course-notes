ac18342ef36cdb76e4a5c96ef505bcb243f7bb61
huntj0n <hunt.jon19@gmail.com>
2023-06-06 23:50:43 -0600

    data-structures initial setup

--- fundamentals/partTwo.js
+++ fundamentals/partTwo.js
@@ -547,23 +547,23 @@ TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
     4.3 Call the function with the 'totals' array
 */
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
 
-const calcTip = (bill) =>
-  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = (bill) =>
+//   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-for (let i = 0; i < bills.length; i++) {
-  tips.push(calcTip(bills[i]));
-  totals.push(bills[i] + tips[i]);
-}
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-};
+// for (let i = 0; i < bills.length; i++) {
+//   tips.push(calcTip(bills[i]));
+//   totals.push(bills[i] + tips[i]);
+// }
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
 
-console.log(bills, tips, totals, calcAverage(totals));
+// console.log(bills, tips, totals, calcAverage(totals));
