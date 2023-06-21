94649962291d0a1c4cf2343a09d87fe758c05710
huntj0n <hunt.jon19@gmail.com>
2023-06-20 21:55:18 -0600

    Spread Operator notes finished

--- data-structures-operators-strings/script.js
+++ data-structures-operators-strings/script.js
@@ -31,6 +31,12 @@ const restaurant = {
       `order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
 };
 
 restaurant.orderDelivery({
@@ -39,6 +45,55 @@ restaurant.orderDelivery({
   mainIndex: 2,
   starterIndex: 2,
 });
+
+//-----THE SPREAD OPERATOR-----
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+console.log(...newArr);
+
+const newMenu = [...restaurant.maiunMenu, "Gnocci"];
+console.log(newMenu);
+//spreading creates a new array from scratch, it doesnt manipulate the original array
+//spreading is similar to destructuring because it also helps us get elements out of arrays. the big difference is that the spread operator takes all the elements from the array and it also doesnt create new variables. As a consequence we can only use it in places where we would separate values by commas.
+
+//two important use cases for the spread operator: shallow copying and merging
+//Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy)
+
+//join 2 arrays together
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//the spread operator works on all iterables: arrays, strings, maps, sets, but NOT objects
+const str = "Jonas";
+const letters = [...str, " ", "s."];
+console.log(letters);
+// console.log(`${...str} Schmedtman`) doesnt work because a template literal isnt expecting multiple things seperated by a comma
+
+//real world example
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1"),
+  prompt("Ingredient 2"),
+  prompt("Ingredient 3"),
+];
+console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+//now, since 2016, the spread operator also works on objects, even though they arent interables
+//Objects
+const newRestaurant = { founding: "1998", ...restaurant, founder: "Guiseppe" };
+console.log(newRestaurant);
+
+//instead of object.assign
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurant.name, restaurantCopy.nane);
 
 //----- DESTRUCTURING ARRAYS -----//
 // const arr = [2, 3, 4];
@@ -77,29 +132,29 @@ restaurant.orderDelivery({
 //
 //-----DESTRUCTURING OBJECTS-----
 //
-const { name, openingHours, categories } = retaurant;
-console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = retaurant;
+// console.log(name, openingHours, categories);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
 
-//default values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+// //default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
 
-//manipulating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj);
-console.log(a, b);
+// //manipulating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log(a, b);
 
-//nested objects
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
+// //nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
