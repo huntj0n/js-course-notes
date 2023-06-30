275d9e42b19004a091413d982affa12c02bdbe5d
huntj0n <hunt.jon19@gmail.com>
2023-06-29 22:08:26 -0600

    rest pattern and parameters

--- data-structures-operators-strings/script.js
+++ data-structures-operators-strings/script.js
@@ -37,6 +37,10 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 restaurant.orderDelivery({
@@ -46,54 +50,90 @@ restaurant.orderDelivery({
   starterIndex: 2,
 });
 
-//-----THE SPREAD OPERATOR-----
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
+restaurant.orderPizza("pepperoni");
 
-const newArr = [1, 2, ...arr];
-console.log(newArr);
-console.log(...newArr);
+//---REST PATTERN AND PARAMETERS-----
+//does the opposite of the spread operator: which was build new arrays, and pass mutiple arguments into a function. In both cases we use it to extend an array into individual elements. The rest pattenr uses the same syntax but to collect multiple elements into an array..
+//spread = on the right side of the assignment operator
+const arr = [1, 2, ...[3, 4]];
+//rest = because its on the left side of the assignment operator
+const [a, b, ...others] = [2, 3, 4, 5];
+console.log(a, b, others);
 
-const newMenu = [...restaurant.maiunMenu, "Gnocci"];
-console.log(newMenu);
-//spreading creates a new array from scratch, it doesnt manipulate the original array
-//spreading is similar to destructuring because it also helps us get elements out of arrays. the big difference is that the spread operator takes all the elements from the array and it also doesnt create new variables. As a consequence we can only use it in places where we would separate values by commas.
-
-//two important use cases for the spread operator: shallow copying and merging
-//Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-// console.log(mainMenuCopy)
-
-//join 2 arrays together
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menu);
-
-//the spread operator works on all iterables: arrays, strings, maps, sets, but NOT objects
-const str = "Jonas";
-const letters = [...str, " ", "s."];
-console.log(letters);
-// console.log(`${...str} Schmedtman`) doesnt work because a template literal isnt expecting multiple things seperated by a comma
-
-//real world example
-const ingredients = [
-  prompt("Let's make pasta! Ingredient 1"),
-  prompt("Ingredient 2"),
-  prompt("Ingredient 3"),
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
 ];
-console.log(ingredients);
+console.log(pizza, risotto, otherFood);
+//notice that the rest operator picks up all elements after the last named variable... the REST of them, it didnt pick up the skipped element above. So you need to make sure that the REST pattern is used at the end and there can only be one rest in any given assignment
 
-// restaurant.orderPasta(...ingredients);
-
-//now, since 2016, the spread operator also works on objects, even though they arent interables
 //Objects
-const newRestaurant = { founding: "1998", ...restaurant, founder: "Guiseppe" };
-console.log(newRestaurant);
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
 
-//instead of object.assign
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = "Ristorante Roma";
-console.log(restaurant.name, restaurantCopy.nane);
+//2) Functions
+const add = function (...numbers) {
+  //using the rest pattern here allows the function to accept an array and all the single values like things are written below
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+//rest arguments = (the numbers above)
+//with rest we compress, with the spread we expand
+
+//-----THE SPREAD OPERATOR-----
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.maiunMenu, "Gnocci"];
+// console.log(newMenu);
+// //spreading creates a new array from scratch, it doesnt manipulate the original array
+// //spreading is similar to destructuring because it also helps us get elements out of arrays. the big difference is that the spread operator takes all the elements from the array and it also doesnt create new variables. As a consequence we can only use it in places where we would separate values by commas.
+
+// //two important use cases for the spread operator: shallow copying and merging
+// //Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// // console.log(mainMenuCopy)
+
+// //join 2 arrays together
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// //the spread operator works on all iterables: arrays, strings, maps, sets, but NOT objects
+// const str = "Jonas";
+// const letters = [...str, " ", "s."];
+// console.log(letters);
+// // console.log(`${...str} Schmedtman`) doesnt work because a template literal isnt expecting multiple things seperated by a comma
+
+// //real world example
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1"),
+//   prompt("Ingredient 2"),
+//   prompt("Ingredient 3"),
+// ];
+// console.log(ingredients);
+
+// // restaurant.orderPasta(...ingredients);
+
+// //now, since 2016, the spread operator also works on objects, even though they arent interables
+// //Objects
+// const newRestaurant = { founding: "1998", ...restaurant, founder: "Guiseppe" };
+// console.log(newRestaurant);
+
+// //instead of object.assign
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = "Ristorante Roma";
+// console.log(restaurant.name, restaurantCopy.nane);
 
 //----- DESTRUCTURING ARRAYS -----//
 // const arr = [2, 3, 4];
