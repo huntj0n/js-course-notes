9fe807e1442bbf9478cae5130509a47c55ee8c3b
huntj0n <hunt.jon19@gmail.com>
2023-07-02 15:49:58 -0600

    Coding Challenge #3

--- data-structures-operators-strings/script.js
+++ data-structures-operators-strings/script.js
@@ -66,46 +66,103 @@ const restaurant = {
   },
 };
 
+//////////////////////////////////////////////////////////
+//-----CODING CHALLENGE #3-----
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "� Yellow card"],
+  [69, "� Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "� Yellow card"],
+]);
+/*
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+*/
+
+//1.
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+
+// //2.
+// gameEvents.delete(64);
+
+// //3.
+// // average = 0;
+// // const minutes = [... new Set(gameEvents.keys())]
+// // for (const minute in minutes){
+// //   average += Number(minute)
+// //   console.log(average)
+// // }
+// // console.log(`An event happened, on average, every ${average/gameEvents.size} minutes`)
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
+// const time = [...gameEvents.keys()].pop();
+// console.log(time);
+// console.log(
+//   `An event happened, on average, every ${time / gameEvents.size} minutes`
+// );
+
+// //4.
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? "FIRST" : "SECOND";
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
+
 //-----MAPS-----
 //in JS, the other new data structure is a map. we use them to map values to keys. similar to objects. but with maps, keys can have any types rather than just strings. leads to some great and andvanced stuff
 
 //Map Iteration
-const question = new Map([
-  ["questions", "What is the best programming language in the world"],
-  [1, "C"],
-  [2, "Java"],
-  [3, "JavaScript"],
-  ["correct", 3],
-  [true, "Correct"][(false, "Try again")],
-]);
-console.log(question); //how this prints out is just like Object.entries(), which means that there is an easy way to convert from Maps to objects!
+// const question = new Map([
+//   ["questions", "What is the best programming language in the world"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "JavaScript"],
+//   ["correct", 3],
+//   [true, "Correct"][(false, "Try again")],
+// ]);
+// console.log(question); //how this prints out is just like Object.entries(), which means that there is an easy way to convert from Maps to objects!
 
-//convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// //convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-//maps are iterables!
+// //maps are iterables!
 
-for (const [key, value] of question) {
-  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
-}
-const answer = 3;
-// const answer = Number(prompt("Your Answer"));
-console.log(answer);
+// for (const [key, value] of question) {
+//   if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+// }
+// const answer = 3;
+// // const answer = Number(prompt("Your Answer"));
+// console.log(answer);
 
-// if (answer === 3) {
-//   console.log(question.get(true));
-// } else console.log(question.get(false ));
+// // if (answer === 3) {
+// //   console.log(question.get(true));
+// // } else console.log(question.get(false ));
 
-console.log(question.get(question.get("correct") === answer));
+// console.log(question.get(question.get("correct") === answer));
 
-//convert map to array
-console.log([...question]);
-//console.log(question.entries()) //will give you a wierd iterator, so you need to spread them here to get a new array without that iterator
-// console.log([...question.entries()]);
-console.log([...question.keys()]);
-console.log([...question.values()]);
+// //convert map to array
+// console.log([...question]);
+// //console.log(question.entries()) //will give you a wierd iterator, so you need to spread them here to get a new array without that iterator
+// // console.log([...question.entries()]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
 
 //Map Fundamentals
 // const rest = new Map();
