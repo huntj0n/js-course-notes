2a2556b100c7e721a7a8d2bc2be10bf92a242f9c
huntj0n <hunt.jon19@gmail.com>
2023-07-02 09:05:42 -0600

    Optional Chaining (?.)

--- data-structures-operators-strings/script.js
+++ data-structures-operators-strings/script.js
@@ -66,6 +66,34 @@ const restaurant = {
   },
 };
 
+//-----OPTIONAL CHAINING (?.)
+// console.log(restaurant.openingHours.mon.open //the 'mon' property is undefined so the open property will return an error
+
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+
+//Example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+  console.log(day);
+  restaurant.openingHours[day]?.open || "closed";
+  console.log(`on ${day}, we open at ${open}`);
+}
+
+//Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+//Arrays
+const users = [{ name: "Jonas", email: "hello@jonas.io" }];
+// const users = []
+
+console.log(users[0]?.name ?? "User arra empty");
+if (users.length > 0) console.log(user[0].name);
+else console.log("user array empty");
+
 //-----ENHANCED OBJECT LITERALS-----
 
 //-----LOOPING ARRAYS: THE FOR-OF LOOP-----
