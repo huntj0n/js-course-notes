f7b45e451718959011c534d66aa1f7eb8b333d23
huntj0n <hunt.jon19@gmail.com>
2023-07-09 13:04:42 -0600

    bind method

--- functions/script.js
+++ functions/script.js
@@ -127,7 +127,7 @@ lufthansa.book(635, "Mike Smith");
 console.log(lufthansa);
 
 const eurowings = {
-  airline: "Eurowings",
+  airLine: "Eurowings",
   iataCode: "EW",
   bookings: [],
 };
@@ -144,7 +144,7 @@ book.call(lufthansa, 2239, "Mary Cooper");
 console.log(lufthansa);
 
 const swiss = {
-  airline: "Swiss Airlines",
+  airLine: "Swiss Airlines",
   iataCode: "LX",
   bookings: [],
 };
@@ -158,3 +158,48 @@ book.apply(swiss, [flightData]);
 console.log(swiss);
 
 book.call(swiss, ...flightData); //using the spread operator here is the exact same as the apply method
+
+//BIND Method
+// book.call(eurowings, 23, "Sarah Williams")
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookSW = book.bind(swiss);
+//does not call the function! just binds it to the eurowings object
+bookEW(23, "Steven Williams");
+console.log(eurowings);
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Charles Oliveira");
+console.log(eurowings);
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * rate
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+
+console.log(addVAT2(100));
+console.log(addVAT2(23));
