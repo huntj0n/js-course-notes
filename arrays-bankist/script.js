ffbf74edf6b602bcebd188cb502eb81a034feb14
huntj0n <hunt.jon19@gmail.com>
2023-07-22 14:39:27 -0600

    the some and every methods

--- arrays-bankist/script.js
+++ arrays-bankist/script.js
@@ -174,6 +174,23 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    //add movement
+    currentAccount.movements.push(amount);
+    //Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -210,6 +227,25 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//----- the .some() and .every() methods-----
+//----SOME----
+console.log(movements);
+//checks for equality
+console.log(movements.includes(-130));
+//checks for any condition
+const anyDeposits = movements.some((mov) => mov > 0);
+console.log(anyDeposits);
+
+//----EVERY----
+console.log(movements.every((mov) => mov > 0));
+console.log(account4.movements.every((mov) => mov > 0));
+
+//separate callback
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
 
 //----- the .find() method -----
 // const firstWithdrawal = movements.find((mov) => mov < 0);
