44a1a5474006a38d17f592bdb0b7274a327710cf
huntj0n <hunt.jon19@gmail.com>
2023-10-13 20:25:16 -0600

    styles, attributes, classes

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -67,3 +67,47 @@ document
   .addEventListener("click", function () {
     message.remove();
   });
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.docmentElement.style.setProperty("--color-primary", "orangeRed");
+
+//Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.designer);
+
+logo.alt = "Beautiful minimalist logo";
+
+//non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute("designer"));
+logo.setAttribute("company", "Bankist");
+
+console.log(logo.src);
+logo.getAttribute("src");
+
+const link = docujent.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+//data attributes
+console.log(logo.dataset.versionNumber);
+
+//Classes
+logo.classList.add("c");
+logo.classList.remobve("c");
+logo.classList.toggle("c");
+logo.classList.containes("c");
