858a197471c8175adb0cf2e5e9be74e986213407
huntj0n <hunt.jon19@gmail.com>
2023-10-31 21:29:39 -0600

    redo: Selecting, Creating, & Deleting Elements. .addEventListener still returning null

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -29,3 +29,53 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
+
+//Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+document.getElementsByClassName("btn");
+
+//Creating and Inserting Elements
+// .insertAdjacentHTML
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = 'We use cookies for improved functionality and analytics.'
+message.innerHTML =
+  "We use cookies for improved functionality and analytics. <button class=btn btn--close-cookie>Got it!</button>";
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message); //these two will insert the element before/after as SIBLING elements
+// header.after(message);
+
+//Delete Elements
+// document
+//   .querySelector(".btn--close-cookie")
+//   .addEventListener("click", function () {
+//     // message.remove();
+//     console.log("clicked");
+//   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector(".btn--close-cookie")
+    .addEventListener("click", function () {
+      // message.remove();
+      console.log("clicked");
+    });
+});
