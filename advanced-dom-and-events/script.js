578dd8203502000171b95ec8c26d8b5a8ec4bf2d
huntj0n <hunt.jon19@gmail.com>
2023-11-02 05:07:21 -0600

    redo smooth-scrolling

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -69,3 +69,40 @@ document
   .addEventListener("click", function () {
     message.remove();
   });
+
+//Smooth Scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+//old school way
+btnScrollTo.addEventListener("click", function (e) {
+  // console.log("clicked");
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log(`Current scroll (X/Y)`, window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //scrolling
+
+  //old-school way
+  // winndow.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
