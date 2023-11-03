21d5006a9bfb69455b13123f6166ccf4acc24219
huntj0n <hunt.jon19@gmail.com>
2023-11-03 04:30:28 -0600

    DOM traversing

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -184,3 +184,34 @@ document
 //   this.style.backgroundColor = randomColor();
 //   console.log("NAV", e.target, e.currentTarget);
 // });
+
+const h1 = document.querySelector("h1");
+
+//Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+//going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+//going sideways: selecting siblings (you can only access direct siblings, previous and the next one)
+connsole.log(h1.previousElementSibling);
+connsole.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) {
+    el.style.transfrom = "scale(0.5)";
+  }
+});
