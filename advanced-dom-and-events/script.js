1126f7f93f5459ca6c88f1c0e8837cdce4aeee8d
huntj0n <hunt.jon19@gmail.com>
2023-11-03 04:50:56 -0600

    building a tabbed component

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -61,6 +61,30 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked);
+
+  //guard clause
+  if (!clicked) return;
+
+  //Active tab
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  clicked.classList.add("operations__tab--active");
+
+  //active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
 /////////////////////////////////////
 /////////////////////////////////////
 /////////////////////////////////////
@@ -68,7 +92,7 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 //Selecting Elements
 // console.log(document.documentElement);
 // console.log(document.head);
-// console.log(document.body);
+// console.log(document.body);o343
 
 const header = document.querySelector(".header");
 const allSections = document.querySelectorAll(".section");
@@ -185,33 +209,33 @@ document
 //   console.log("NAV", e.target, e.currentTarget);
 // });
 
-const h1 = document.querySelector("h1");
+// const h1 = document.querySelector("h1");
 
-//Going downwards: child
-console.log(h1.querySelectorAll(".highlight"));
-console.log(h1.childNodes);
-console.log(h1.children);
-h1.firstElementChild.style.color = "white";
-h1.lastElementChild.style.color = "orangered";
+// //Going downwards: child
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = "white";
+// h1.lastElementChild.style.color = "orangered";
 
-//going upwards: parents
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// //going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest(".header").style.background = "var(--gradient-secondary)";
+// h1.closest(".header").style.background = "var(--gradient-secondary)";
 
-h1.closest("h1").style.background = "var(--gradient-primary)";
+// h1.closest("h1").style.background = "var(--gradient-primary)";
 
-//going sideways: selecting siblings (you can only access direct siblings, previous and the next one)
-connsole.log(h1.previousElementSibling);
-connsole.log(h1.nextElementSibling);
+// //going sideways: selecting siblings (you can only access direct siblings, previous and the next one)
+// connsole.log(h1.previousElementSibling);
+// connsole.log(h1.nextElementSibling);
 
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
 
-console.log(h1.parentElement.children);
-[...h1.parentElement.children].forEach(function (el) {
-  if (el !== h1) {
-    el.style.transfrom = "scale(0.5)";
-  }
-});
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) {
+//     el.style.transfrom = "scale(0.5)";
+//   }
+// });
