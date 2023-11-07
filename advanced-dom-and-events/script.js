070fe01e1a9cc6d03dd261663334ec24ce7e1bbf
huntj0n <hunt.jon19@gmail.com>
2023-11-06 18:52:32 -0700

    Intersection Observer API reveal sections on scroll

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -157,6 +157,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+//Reveal Sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 /////////////////////////////////////
 /////////////////////////////////////
 /////////////////////////////////////
@@ -167,7 +189,7 @@ headerObserver.observe(header);
 // console.log(document.body);o343
 
 // const header = document.querySelector(".header");
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 // console.log(allSections);
 document.getElementById("section--1");
 const allButtons = document.getElementsByTagName("button");
