146b6d26e7f87d460d986fb244f1ed96c56c1da7
huntj0n <hunt.jon19@gmail.com>
2023-11-06 21:35:49 -0700

    lazy loading images with Intersection Observer API

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -179,6 +179,32 @@ allSections.forEach(function (section) {
   section.classList.add("section--hidden");
 });
 
+//Lazy Loading Images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  //replace img with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
 /////////////////////////////////////
 /////////////////////////////////////
 /////////////////////////////////////
