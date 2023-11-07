e0c522203f6f2bbe5b86af814153634690c3e2b4
huntj0n <hunt.jon19@gmail.com>
2023-11-06 17:12:49 -0700

    nav menu handleHover fix

--- advanced-dom-and-events/script.js
+++ advanced-dom-and-events/script.js
@@ -86,19 +86,36 @@ tabsContainer.addEventListener("click", function (e) {
 });
 
 //Menu Fade Animation
-nav.addEventListener("mouseover", function (e) {
+// nav.addEventListener("mouseover", function (e) {
+//   if (e.target.classList.contains("nav__link")) {
+//     const link = e.target;
+//     const siblings = link.closest(".nav").querySelectorAll("nav__link");
+//     const logo = link.closest(".nav").querySelector("img");
+
+//     siblings.forEach((el) => {
+//       if (el !== link) el.style.opacity = 0.5;
+//     });
+//     logo.style.opacity = 0.5;
+//   }
+// });
+// nav.addEventListener("mouseout", function (e) {});
+
+const handleHover = function (e) {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
-    const siblings = link.closest(".nav").querySelectorAll("nav__link");
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
     const logo = link.closest(".nav").querySelector("img");
 
     siblings.forEach((el) => {
-      if (el !== link) el.style.color = red;
+      if (el !== link) el.style.opacity = this;
     });
-    logo.style.color = red;
+    logo.style.opacity = this;
   }
-});
-nav.addEventListener("mouseout", function (e) {});
+};
+
+// Passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
 
 /////////////////////////////////////
 /////////////////////////////////////
