787cebe75b13de7ec5f759795aa127f3546242b5
huntj0n <hunt.jon19@gmail.com>
2023-11-07 20:50:57 -0700

    ES6 Classes

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -12,7 +12,7 @@ const Person = function (firstName, birthyear) {
 };
 
 const jonas = new Person("Jonas", 1991);
-console.log(jonas);
+// console.log(jonas);
 
 //4 steps behind the scenes
 //1. New {} is created
@@ -22,17 +22,17 @@ console.log(jonas);
 
 const matilda = new Person("Matilda", 2017);
 const jack = new Person("Jack", 1975);
-console.log(matilda, jack);
+// console.log(matilda, jack);
 
 const jay = "Jay";
 
-console.log(jonas instanceof Person);
-console.log(jay instanceof Person);
+// console.log(jonas instanceof Person);
+// console.log(jay instanceof Person);
 
 /////////////////////////
 //PROTOTYPES
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthyear);
@@ -41,40 +41,72 @@ Person.prototype.calcAge = function () {
 jonas.calcAge();
 matilda.calcAge();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(jonas));
 
 Person.prototype.species = "Homo Sapiens";
-console.log(jonas.species, matilda.species);
+// console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasOwnProperty("firstName"));
-console.log(jonas.hasOwnProperty("species"));
+// console.log(jonas.hasOwnProperty("firstName"));
+// console.log(jonas.hasOwnProperty("species"));
 
 ////////////////
 //Prototypal Inheritance on Built-in Objects
-console.log(jonas.__proto__);
-console.log(jonas.__proto__.__proto__); //Object.prototype is usually the top of the prototype chain
-console.log(jonas.__proto__.__proto__.__proto__); //null
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__.__proto__); //Object.prototype is usually the top of the prototype chain
+// console.log(jonas.__proto__.__proto__.__proto__); //null
 
-console.log(Person.prototype.constructor);
-console.dir(Person.prototype.constructor);
+// console.log(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
 
 const arr = [3, 6, 4, 5, 6, 9, 3, 6, 9, 7, 5];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__.__proto__);
 
 Array.prototype.unique = function () {
   [...new Set(this)];
 };
-console.log(arr.unique);
+// console.log(arr.unique);
 
 //the prototype chain for html elements
 const h1 = document.querySelector("h1");
-console.dir(h1);
+// console.dir(h1);
 
-console.dir((x) => x + 1);
+// console.dir((x) => x + 1);
+
+///////
+// ES6 Classes
+
+const PersonClexp = class {}; //class expression
+class PersonCldec {} //class declaration
+
+class PersonCl {
+  constructor(firstName, birthyear) {
+    this.firstName = firstName;
+    this.birthyear = birthyear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthyear);
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+jessica.greet();
+
+//1. Classes ARE NOT hoisted, even if they are class declarations
+//2. Classes, like functions, are first class citizens
+//3. the body of a Class is executed in strict mode, even if we dont declare it
 
 ////////////////////////////////
 // Coding Challenge #1
@@ -92,28 +124,28 @@ DATA Car 1: 'BMW' going at 120 km/h
 DATA Car 2: "Mercedes" going at 95 km/h
 */
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed = this.speed + 10;
-  console.log(this.speed);
-};
-Car.prototype.brake = function () {
-  this.speed = this.speed - 5;
-  console.log(this.speed);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed = this.speed + 10;
+//   console.log(this.speed);
+// };
+// Car.prototype.brake = function () {
+//   this.speed = this.speed - 5;
+//   console.log(this.speed);
+// };
 
-const bmw = new Car("BMW", 120);
-const mercedes = new Car("Mercedes", 95);
+// const bmw = new Car("BMW", 120);
+// const mercedes = new Car("Mercedes", 95);
 
-bmw.accelerate();
-bmw.brake();
-bmw.brake();
+// bmw.accelerate();
+// bmw.brake();
+// bmw.brake();
 
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.brake();
-mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.brake();
+// mercedes.accelerate();
