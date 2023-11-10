7f1901c8baed23dcb5723a89f8eff9b187801a95
huntj0n <hunt.jon19@gmail.com>
2023-11-10 11:45:21 -0700

    setters and getters

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -83,30 +83,66 @@ const PersonClexp = class {}; //class expression
 class PersonCldec {} //class declaration
 
 class PersonCl {
-  constructor(firstName, birthyear) {
-    this.firstName = firstName;
+  constructor(fullName, birthyear) {
+    this.fullName = fullName;
     this.birthyear = birthyear;
   }
 
   calcAge() {
     console.log(2037 - this.birthyear);
   }
+  get age() {
+    return 2037 - this.birthyear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this.fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl("Jessica", 1996);
+const jessica = new PersonCl("Jessica Davis", 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
 PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
+  console.log(`Hey ${this._firstName}`);
 };
 jessica.greet();
 
 //1. Classes ARE NOT hoisted, even if they are class declarations
 //2. Classes, like functions, are first class citizens
 //3. the body of a Class is executed in strict mode, even if we dont declare it
+
+////////
+// Setters and Getters
+
+//functions that get or set a value
+
+const account = {
+  owner: "Jonas",
+  movments: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
 
 ////////////////////////////////
 // Coding Challenge #1
