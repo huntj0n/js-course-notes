ed7fb39eec62ac9ceba828b9bd1a7392c42f5cbe
huntj0n <hunt.jon19@gmail.com>
2023-11-10 19:27:54 -0700

    chaining methods

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -420,6 +420,7 @@ console.log(acc1.pin);
 //Private methods
 // (there is also the static version)
 
+/*
 class Account {
   // 1) Public fields (instances)
   locale = navigator.language;
@@ -481,6 +482,73 @@ Account.helper();
 // console.log(acc1.#movements);
 // console.log(acc1.#pin);
 // console.log(acc1.#approveLoan(100));
+*/
+
+//
+//
+//  CHAINING METHODS
+//
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    console.log(` Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  //Public Interface
+  getMovements() {
+    return this.#movements;
+  }
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+    }
+    return this;
+  }
+
+  static helper() {
+    console.log(`Helper`);
+  }
+
+  // 4) Private Methods: browsers dont fully support these yet
+  //   #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+const acc1 = new Account("Jonas", "EUR", 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+//Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).deposit(4000);
+console.log(acc1.getMovements());
 
 ////////////////////////////////
 // Coding Challenge #1
