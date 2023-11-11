5e181d961d22d2473de6419dcf10e1eb484f68d3
huntj0n <hunt.jon19@gmail.com>
2023-11-10 17:53:28 -0700

    coding challenge 3

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -209,6 +209,7 @@ console.log(mike.__proto__.__proto__);
 
 console.log(mike instanceof Student);
 console.log(mike instanceof Person);
+console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
@@ -299,3 +300,57 @@ ford.brake();
 ford.speedUS = 50;
 console.log(ford);
 */
+
+////////////////////////////////
+// Coding Challenge #2
+
+/*
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD 'class' of Car. Besides a make and current speed, the EV also has the current batter charge in % ('charge' property);
+2. Implement a 'chargeBattery method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase teh car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%;
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HUNT: Review the definition of polymporphism;
+
+DATA CAR 1: 'TESLA going at 120 km/h, with a charge of 23%
+*/
+
+//Car class setup
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(this.speed);
+};
+
+//////// challenge code ///////
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+
+tesla.brake();
+tesla.accelerate();
