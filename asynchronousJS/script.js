e98e1a6e448daf6e81e43bc4580af74a135ee41a
huntj0n <hunt.jon19@gmail.com>
2023-11-11 10:11:18 -0700

    building a simple promise

--- asynchronousJS/script.js
+++ asynchronousJS/script.js
@@ -247,3 +247,33 @@ GOOD LUCK �
 // });
 
 // console.log("test end");
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log("lottery draw is happening");
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve(" You WIN!");
+    } else {
+      reject(new Error("You lost your money :("));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then((res) => console.log(res)).catch((err) => console.log(err));
+
+//promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log("I waited for 2 seconds");
+    return wait(1);
+  })
+  .then(() => console.log("i waited for 1 second"));
+
+Promise.resolve("abc").then((x) => console.log(x));
+Promise.reject(new Error("Problem!")).then((x) => console.error(x));
