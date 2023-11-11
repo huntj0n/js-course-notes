072661751d0757149f9f72c0d80dcfc2ad9375b9
huntj0n <hunt.jon19@gmail.com>
2023-11-10 19:08:59 -0700

    Encapsulation: Protected Properties and Methods

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -310,6 +310,7 @@ jay.introduce();
 jay.calcAge();
 */
 
+/*
 //
 //
 // ANOTHER CLASS EXAMPLE
@@ -352,6 +353,62 @@ acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 console.log(acc1);
+
+*/
+
+/*
+//
+//
+//ENCAPSULATION: PROTECTED PROPERTIES AND METHODS
+//
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    //protected property
+    this._pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(` Thanks for opening an account, ${owner}`);
+  }
+
+  //Public Interface
+  getMovements() {
+    return this._movements;
+  }
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+
+acc1._movements.push(250);
+acc1._movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1.pin);
+*/
 
 ////////////////////////////////
 // Coding Challenge #1
