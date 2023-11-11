bcab642e2123533c7dbd266cd254315546000097
huntj0n <hunt.jon19@gmail.com>
2023-11-10 19:23:28 -0700

    Encapsulation: Private Class Fields and Methods

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -410,6 +410,78 @@ console.log(acc1);
 console.log(acc1.pin);
 */
 
+//
+//
+// ENCAPSULATION: PRIVATE CLASS FIELDS AND METHODS
+//
+//Public fields
+//Private fields
+//Public methods
+//Private methods
+// (there is also the static version)
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    //protected property
+    this.#pin = pin;
+    //   this._movements = [];
+    //   this.locale = navigator.language;
+
+    console.log(` Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  //Public Interface
+  getMovements() {
+    return this.#movements;
+  }
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+    }
+  }
+
+  static helper() {
+    console.log(`Helper`);
+  }
+
+  // 4) Private Methods: browsers dont fully support these yet
+  //   #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+const acc1 = new Account("Jonas", "EUR", 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
 ////////////////////////////////
 // Coding Challenge #1
 
