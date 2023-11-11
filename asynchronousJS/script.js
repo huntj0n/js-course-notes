49a2f25f2e1fe12abf1ae300510d48dd0c4b30fb
huntj0n <hunt.jon19@gmail.com>
2023-11-11 11:25:42 -0700

    running priomises in parallel

--- asynchronousJS/script.js
+++ asynchronousJS/script.js
@@ -507,6 +507,7 @@ console.log("FIRST");
 
 */
 
+/*
 ///////////////////////////////////////
 // Returning Values from Async Functions
 const getPosition = function () {
@@ -562,3 +563,32 @@ console.log("1: Will get location");
   }
   console.log("3: Finished getting location");
 })();
+
+*/
+
+///////////////////////////////////////
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+    console.log(data.map((d) => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries("portugal", "canada", "tanzania");
