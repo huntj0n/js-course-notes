18afd55295d7a4866e8b295a671ae9a3f6e6fcf1
huntj0n <hunt.jon19@gmail.com>
2023-11-10 19:00:56 -0700

    another class example

--- object-oriented-programming/script.js
+++ object-oriented-programming/script.js
@@ -279,6 +279,7 @@ martha.introduce();
 martha.calcAge();
 */
 
+/*
 //
 //
 //INHERITANCE BETWEEN 'CLASSES': OBJECT.CREATE
@@ -307,6 +308,50 @@ const jay = Object.create(StudentProto);
 jay.init("Jay", 2010, "Computer Science");
 jay.introduce();
 jay.calcAge();
+*/
+
+//
+//
+// ANOTHER CLASS EXAMPLE
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner;
+    this.currency;
+    this.pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(` Thanks for opening an account, ${owner}`);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1);
 
 ////////////////////////////////
 // Coding Challenge #1
